35b611f6d40071558a6affb67dc932cd305da0dc
tahmid-khan-hub <tahmidkhan0011@gmail.com>
2026-02-27 03:17:16 +0600

    Updated the Navbar structure and primary colour from global css

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -2,6 +2,8 @@
 import Logo from "@/components/Logo/Logo";
 import NavLinks from "./components/NavLinks";
 import MobileMenu from "./components/MobileMenu";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Navbar() {
   return (
@@ -15,8 +17,13 @@ export default function Navbar() {
           <NavLinks />
         </div>
 
-        {/* Mobile Menu */}
-        <MobileMenu />
+        <div className="flex items-center gap-3">
+          {/* Login or Log out button */}
+          <Link href="/login"><Button className="bg-purple-300 text-primary-foreground hover:bg-purple-400/90 mr-1 mt-1.5" variant="outline">Default</Button></Link>
+
+          {/* Mobile Menu */}
+          <MobileMenu />
+        </div>
       </div>
     </div>
   );
